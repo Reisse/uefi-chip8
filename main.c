356e046fea6e8efd6ad1c122e8aa41bbfa8f3845
Reisse <babayants.alexander@gmail.com>
2017-01-22 04:25:20 +0300

    Fixed missing calls to display_init & inputt_init

--- main.c
+++ main.c
@@ -23,13 +23,27 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 	else
 		i_status = interpreter_init(argv[2], argv[1], &i_state);
 
-	if (i_status != INT_OK) {
+	if (i_status != INT_OK)
+	{
 		Print(L"Failed to initialize interpreter.\n");
 		return EFI_SUCCESS;
 	}
 
+	if (input_init() != IN_OK)
+	{
+		Print(L"Failed to initialize keyboard input.\n");
+		return EFI_SUCCESS;
+	}
+
+	if (display_init() != DS_OK)
+	{
+		Print(L"Failed to initialize display.\n");
+		return EFI_SUCCESS;
+	}
+
 	i_status = interpreter_loop(&i_state);
-	if (i_status != INT_OK) {
+	if (i_status != INT_OK)
+	{
 		Print(L"Failed in interpreter loop.\n");
 	}
 
